37f3150a08d5ecf34411a05b328ae8f71cd80d92
aalexanddrova1 <91866423+aalexanddrova@users.noreply.github.com>
2022-01-17 07:11:33 +0000

    prog

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-teksts = input("ievadiet cipari virkne: ")
-
-def countZeros(teksts):
-  return zeros
-
-teksts = ""
-print(countZeros(teksts))
-countZeros(teksts)
+teksts = input("ievadiet tekstu:")
+def replaceTwos(teksts):
+  if teksts.count("2")>0:
+    teksts = teksts.replace("2","divi")
+    print(teksts)
+  else:
+   teksts = "nekas netika aizvietots"
+  print(teksts)
+  return teksts
+replaceTwos(teksts)
